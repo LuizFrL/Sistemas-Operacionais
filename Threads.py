489ad4f9f3fd79d3b70a8d9e73561d0b43431810
Luiz Fernando <luizfernandorole@gmail.com>
2019-11-11 22:32:46 -0300

    Implementação do método Lock, usado para proteger uma variavel para ser acessivél apenas para uma Thread

---
+++ Threads.py
@@ -0,0 +1,41 @@
+import threading as _
+
+
+class Conta(object):
+
+    def __init__(self, nome=None, saldo=0):
+        self.saldo = saldo
+        self.cliente = nome
+
+    def get_saldo(self):
+        return self.saldo
+
+    def get_nome(self):
+        return self.cliente
+
+    def transfere(self, conta, transferencia, protect):
+        protect.acquire()
+        try:
+            if self.saldo >= transferencia:
+                conta.saldo += transferencia
+                self.saldo -= transferencia
+                print(self.get_nome(), 'tem', self.get_saldo())
+                print(conta.get_nome(), 'tem', conta.get_saldo())
+        finally:
+            protect.release()
+
+if __name__ == '__main__':
+    joao = Conta('Joao Pedro Felipe', 10000)
+    claudia = Conta('Claudia Nascimento Souza', 0)
+
+    num_tread = 100
+
+    thrs = [ ]
+
+    lock = _.Lock()
+
+    for threads in range(num_tread):
+        thread = _.Thread(target=joao.transfere, args=(claudia, 10, lock))
+        thread.start()
+        thrs.append(thread)
+
